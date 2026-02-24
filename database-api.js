4d81dabaf1847c52a279141d9642418a7e9185d0
Ryan <ryananicettshovu@gmail.com>
2026-02-24 17:01:09 +0200

    Created database API module.

---
+++ database-api.js
@@ -0,0 +1,90 @@
+const sbClient = supabase.createClient(
+    "https://cbkttelrpoizjcxbiygh.supabase.co",
+    "sb_publishable_0qhnYXy2bmdbxfxq0Hpovg_DEQNf5-_", {
+    db: {
+        schema: "public"
+    }
+});
+
+async function runQuery(queryFunction) {
+    const response = await queryFunction(sbClient);
+    return {
+        then: (callback) => { callback(response) }
+    }
+}
+
+function reformatResponse(response, isDataAnArray = false) {
+    console.log(response);
+    let res = {};
+    if (response.error) {
+        res.error = "[Database API] Unexpected error: " + response.error.message;
+    }
+    if (response.data) {
+        res.data = isDataAnArray ? response.data : response.data[0];
+    } else {
+        res.data = true;
+    }
+    return res
+}
+
+const userColumnsExceptPassword = "id, first_name, last_name, username, bio";
+
+const dbApi = {
+    getAllUsers: async () => {
+        return reformatResponse(await sbClient.from("user").select("*"))
+    },
+
+    createNewUser: async (newUser) => {
+        return reformatResponse(await sbClient.from("user").insert(newUser))
+    },
+
+    getUserById: async (id) => {
+        return reformatResponse(await sbClient.from("user").select(userColumnsExceptPassword).eq("id", id))
+    },
+
+    getUserAttributeById: async (attribute, id) => {
+        return reformatResponse(await sbClient.from("user").select(attribute).eq("id", id))
+    },
+
+    setUserById: async (newUser, id) => {
+        return reformatResponse(await sbClient.from("user").update(newUser).eq("id", id))
+    },
+
+    setUserAttributeById: async (attribute, id) => {
+        return reformatResponse(await sbClient.from("user").update(attribute).eq("id", id))
+    },
+
+    deleteUserById: async (id) => {
+        return reformatResponse(await sbClient.from("user").delete("*").eq("id", id))
+    },
+
+    createNewSessionFromLogin: async (username, password) => {
+        const user_id = reformatResponse(await sbClient.from("user").select("id")
+            .eq("username", username)
+            .eq("password", password)
+        ).data.id;
+
+        if (!user_id) {
+            return {
+                error: "[Database API] Error: Invalid login",
+                data: false,
+            }
+        }
+
+        return reformatResponse(await sbClient.from("session").insert({ "user_id": user_id }))
+    },
+
+    getUserIdBySessionId: async (id) => {
+        return reformatResponse(await sbClient.from("session").select("user_id").eq("id", id));
+    },
+
+    deleteSessionById: async (id) => {
+        return reformatResponse(await sbClient.from("session").delete("*").eq("id", id))
+    },
+
+    deleteAllSessions: async () => {
+        return reformatResponse(await sbClient.from("session").delete("*").not("id", "is", null))
+    },
+};
+
+export { dbApi }
