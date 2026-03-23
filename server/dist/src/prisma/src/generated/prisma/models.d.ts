5a0309b71ae6206fe8dccb149b39a46106d90422
NhyiraAmofaSekyi <nhyiraamofasekyi@gmail.com>
2026-03-23 21:26:24 +0200

    Stop tracking dist folder

--- server/dist/src/prisma/src/generated/prisma/models.d.ts
+++
@@ -1,8 +0,0 @@
-export type * from './models/User.js';
-export type * from './models/UserAllocation.js';
-export type * from './models/Message.js';
-export type * from './models/Meeting.js';
-export type * from './models/File.js';
-export type * from './models/BlogPost.js';
-export type * from './models/Notification.js';
-export type * from './commonInputTypes.js';
--- server/dist/src/tutor/tutor.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { UserService } from 'src/user/user.service';
-export declare class TutorController {
-    private userService;
-    constructor(userService: UserService);
-    hello(): string;
-}
