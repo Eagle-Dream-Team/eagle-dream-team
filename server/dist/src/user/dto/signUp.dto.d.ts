24c1b932eac6a713dfddd682bfde2510c0a9841c
Ryan <ryananicettshovu@gmail.com>
2026-03-23 23:19:14 +0200

    gitignore dist

--- server/dist/src/user/dto/signUp.dto.d.ts
+++
@@ -1,8 +0,0 @@
-import { Role } from 'src/generated/prisma/client';
-export declare class SignUpDto {
-    email: string;
-    password: string;
-    firstName: string;
-    lastName: string;
-    role: Role;
-}
