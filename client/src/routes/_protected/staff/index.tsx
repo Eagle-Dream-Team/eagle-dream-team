a6dfffdc09b7c97a9ff364b33965d5ed3fc35db1
Ryan <ryananicettshovu@gmail.com>
2026-03-27 22:36:00 +0200

    Removed cache.

--- client/src/routes/_protected/staff/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_protected/staff/")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>Hello staff member</div>;
-}
